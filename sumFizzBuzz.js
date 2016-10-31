11df14aa8393e4e83e892cd19ae6f1ba0aab601b
Eugene Dobry <edory@gmail.com>
2016-10-31 01:00:07 -0400

    much more performant analytic solution

--- sumFizzBuzz.js
+++ sumFizzBuzz.js
@@ -1,15 +1,62 @@
-var isMultipleOf = n => x => x % n == 0;
+var flatten = list => list.reduce((arr, curr) => arr.concat(curr), []);
 
-var sumFizzBuzz = (n, intervals) => {
-    var multipleChecks = intervals.map(isMultipleOf);
-    var isMultiple = x => multipleChecks.some(check => check(x));
-    var total = 0;
-    for(var i = 1; i < n; i++) {
-        if(isMultiple(i))
-            total += i;
-    }
+var numericSort = list => list.map(x => x).sort((a, b) => a - b);
 
-    return total;
+var chain = (firstStep, ...steps) => (...args) => steps.reduce((prevResult, step) =>
+    step(prevResult),
+    firstStep(...args));
+
+var combinationsOf2 = set => {
+    if(set.length == 2)
+        return [set];
+
+    var [head, ...rest] = set;
+    return [
+        ...rest.map(x => [head, x]),
+        ...combinationsOf2(rest)];
+};
+
+var findLCMs = list => combinationsOf2(list).map(([a, b]) => a * b);
+
+var dividesEvenly = (dividend, divisor) => dividend % divisor == 0;
+var isMultipleOf = n => x => dividesEvenly(n, x);
+var isFactor = x => n => dividesEvenly(x, n);
+
+var termial = n => (Math.pow(n, 2) + n) / 2;
+var intQuotient = n => x => Math.floor(n / x);
+
+var sumOfMultiplesUpTo = n => {
+    var numMultiples = chain(
+        intQuotient(n - 1),
+        termial);
+
+    return x => numMultiples(x) * x;
+};
+
+var collapseMultiples = list => numericSort(list).reduce((terms, currentTerm) => {
+    if(!terms.some(isFactor(currentTerm)))
+        terms.push(currentTerm);
+    return terms;
+}, []);
+
+var totalMultipleSum = summer => list => list.map(summer).reduce((a, b) => a + b, 0)
+
+var sumFizzBuzz = (n, terms) => {
+    var collapsedTerms = collapseMultiples(terms);
+
+    var multipleSummer = chain(
+        sumOfMultiplesUpTo,
+        totalMultipleSum
+    )(n);
+
+    var multipleSum = multipleSummer(collapsedTerms);
+
+    var redundantMultipleSum = chain(
+        findLCMs,
+        multipleSummer
+    )(collapsedTerms);
+
+    return multipleSum - redundantMultipleSum;
 };
 
 module.exports = sumFizzBuzz;
